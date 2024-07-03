6295fe20acf5f8da782ab1d01229ff9a81341553
gmaiwun <gmaiwun@gmail.com>
2024-07-02 20:54:00 -0500

    Initial commit

---
+++ bucket_policy.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket_policy" "gmai_bucket_a_public_policy" {
+  bucket = aws_s3_bucket.bucket_a.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.bucket_a.arn}/*"
+      }
+    ]
+  })
+}
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "website_url" {
+  value = aws_s3_bucket.bucket_a.website_endpoint
+  description = "The URL of the website hosted on the S3 bucket"
+}

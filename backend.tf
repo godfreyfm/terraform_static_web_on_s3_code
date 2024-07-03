9738b86fc4da6275c830a227e69cb48c4cd5dd7d
gmaiwun <gmaiwun@gmail.com>
2024-07-03 17:09:53 -0500

    created backend 2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sbox-remote-backend "
+    bucket = "sbox-remote-backend"
     key = "sbox-remote-backend/tfstate"
     dynamodb_table = "my_lock_table"
     encrypt = true
